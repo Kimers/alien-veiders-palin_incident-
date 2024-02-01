bcfd5fd41398e65ecfc4a66449caedf21d8b5e77
Sudi <timohiniliusha@yandex.ru>
2024-02-01 16:40:51 +0300

    Обьединил настройки с игрой, фон заработал

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import sys
 import pygame
+from setings import Settings
 
 class Game():
     def __init__(self):
         pygame.init()
-        
-        self.screen = pygame.display.set_mode((800,600))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("Атака пришельцев")
-        self.bg_color = (41, 179, 217)
+        #self.bg_color = (41, 179, 217)
         
     def run_game(self):
         
@@ -16,9 +17,9 @@ class Game():
                 if event.type == pygame.QUIT:
                     sys.exit()
         
-        self.screen.fill(self.bg_color)
-                    
-        pygame.display.flip()
+            self.screen.fill(self.settings.bg_color)
+        
+            pygame.display.flip()
         
 
 
--- setings.py
+++ setings.py
@@ -2,4 +2,4 @@ class Settings():
     def __init__(self):
         self.screen_width = 800
         self.screen_height = 600
-        self.bg_color = (230,230,230)
+        self.bg_color = (128, 130, 152)
