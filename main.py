74448a10ca8ea77badd76e798f9279911024ca60
Sudi <timohiniliusha@yandex.ru>
2024-02-02 00:02:17 +0300

    Упрастил метод run_game разбив его на два метода

--- main.py
+++ main.py
@@ -13,18 +13,21 @@ class Game():
         #self.bg_color = (41, 179, 217)
         
     def run_game(self):
-        
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-        
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-        
-            pygame.display.flip()
-        
+            self._check_event()
+            self._update_screen()
 
+        
+    def _check_event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()        
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()        
+        
 
 if __name__ == '__main__':
     ai = Game()
