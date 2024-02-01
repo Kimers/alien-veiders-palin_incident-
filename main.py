f4cd20cff5b9dd4e0348a5473e9813ae0994a67a
Sudi <timohiniliusha@yandex.ru>
2024-02-01 15:36:20 +0300

    Не меняестся фон но возможно скоро поменяется!

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ class Game():
     def __init__(self):
         pygame.init()
         
-        self.window = pygame.display.set_mode((800,600))
+        self.screen = pygame.display.set_mode((800,600))
         pygame.display.set_caption("Атака пришельцев")
         self.bg_color = (41, 179, 217)
         
-    def ran_game(self):
+    def run_game(self):
         
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
         
-        self.window.fill(self.bg_color)
+            self.screen.fill(self.bg_color)
                     
         pygame.display.flip()
         
@@ -24,4 +24,4 @@ class Game():
 
 if __name__ == '__main__':
     ai = Game()
-    ai.ran_game()
+    ai.run_game()
